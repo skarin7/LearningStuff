36237935ac7b76eeecce1f99c70e709a41b5a71d
Shankar <shankar.karingula@gmail.com>
2024-10-20 17:01:26 +0530

    prod consumer with controlled producing and waiting..

--- src/main/java/com/practice/multithreading/ProducerConsumerTest.java
+++ src/main/java/com/practice/multithreading/ProducerConsumerTest.java
@@ -1,65 +1,131 @@
 package com.practice.multithreading;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Vector;
 
 public class ProducerConsumerTest {
 
-    static volatile List<Integer> sharedObject = new Vector<>();
+    public static void main(String[] args) throws InterruptedException {
+        List<Integer> sharedObject = new ArrayList<>();
+        Object lock = new Object(); // Shared lock object for synchronization
 
-    public static void main(String[] args) {
+        MyProducer prod = new MyProducer(sharedObject, 5, lock);
+        Thread producerThread = new Thread(prod);
 
-        MyProducer prod = new MyProducer();
-        MyConsumer consume = new MyConsumer();
-        prod.start();
-        consume.start();
+        MyConsumer consume = new MyConsumer(sharedObject, 5, lock);
+        Thread consumerThread = new Thread(consume);
 
+        producerThread.start();
+        consumerThread.start();
+
+        producerThread.join();
+        consumerThread.join();
     }
 
+    static class MyProducer implements Runnable {
+        final List<Integer> sharedObject;
+        int maxSize;
+        final Object lock;
 
-
-    static class MyProducer extends Thread {
+        public MyProducer(List<Integer> sharedObject, int maxSize, Object lock) {
+            this.sharedObject = sharedObject;
+            this.maxSize = maxSize;
+            this.lock = lock;
+        }
 
         @Override
         public void run() {
             System.out.println("Producer producing !!");
-                for (int i = 1; i < 100; i++) {
-                //Thread.sleep(500);
-                    sharedObject.add(i);
-                    if(sharedObject.size() > 4) {
-                        sharedObject.remove(sharedObject.size() - 1);
+
+            while (true) {
+                synchronized (lock) {
+                    // Wait if buffer is full
+                    while (sharedObject.size() == maxSize) {
+                        System.out.println("Buffer is full, waiting...");
+                        try {
+                            lock.wait(); // Wait until consumer consumes items
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
 
+                    // Produce items if buffer is not full
+                    for (int i = 0; i < maxSize; i++) {
+                        sharedObject.add(i);
+                        System.out.println("Produced: " + i);
+                    }
+
+                    // Notify the consumer that items are ready
+                    System.out.println("Buffer is full, notifying consumer...");
+                    lock.notify();
+
+                    try {
+                        // Simulate time taken to produce
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
                 }
-
+            }
         }
-
     }
 
-    static class MyConsumer extends Thread {
+    static class MyConsumer implements Runnable {
+        final List<Integer> sharedObject;
+        int maxSize;
+        final Object lock;
+
+        public MyConsumer(List<Integer> sharedObject, int maxSize, Object lock) {
+            this.sharedObject = sharedObject;
+            this.maxSize = maxSize;
+            this.lock = lock;
+        }
 
         @Override
         public void run() {
-            System.out.println("Consmer consuming!!");
-            try {
-                Thread.sleep(500);
-//                while (true) {
-                    synchronized (sharedObject) {
-                        Iterator<Integer> iterator = sharedObject.iterator();
-                        while (iterator.hasNext()) {
-                            System.out.println(iterator.next());
+            System.out.println("Consumer consuming!!");
+
+            while (true) {
+                synchronized (lock) {
+                    // Wait if buffer is empty
+                    while (sharedObject.isEmpty()) {
+                        System.out.println("Buffer is empty, waiting...");
+                        try {
+                            lock.wait(); // Wait until producer produces items
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
                         }
                     }
-//                }
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
 
-            }
+                    // Consume items if buffer is not empty
+                    Iterator<Integer> iterator = sharedObject.iterator();
+                    while (iterator.hasNext()) {
+                        Integer item = iterator.next();
+                        System.out.println("Consumed: " + item);
+                    }
 
+                    try {
+                        System.out.println(" Consuming all the events.. and take rest for sometime before asking prodcuer to producer");
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    // Clear the buffer
+                    sharedObject.clear();
+                    System.out.println("Buffer is empty, notifying producer...");
+
+                    // Notify the producer that buffer is empty and they can produce
+                    lock.notify();
+
+                    try {
+                        // Simulate time taken to consume
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
         }
-
-
+    }
 }
